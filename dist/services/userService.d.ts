3a155d3692267e156dc12e912c852496c26e9b68
rs029 <rs029singh@gmail.com>
2025-10-08 09:22:08 +0530

    moved server.ts into src folder

--- dist/services/userService.d.ts
+++ dist/services/userService.d.ts
@@ -1,15 +1,15 @@
 export declare const getUsers: () => import(".prisma/client").Prisma.PrismaPromise<{
-    name: string;
     id: number;
     email: string;
+    name: string;
     password: string;
     createdAt: Date;
     updatedAt: Date;
 }[]>;
 export declare const createUser: (name: string, email: string, password: string) => Promise<{
-    name: string;
     id: number;
     email: string;
+    name: string;
     password: string;
     createdAt: Date;
     updatedAt: Date;
@@ -17,9 +17,9 @@ export declare const createUser: (name: string, email: string, password: string)
 export declare const loginUser: (email: string, password: string) => Promise<{
     token: string;
     user: {
-        name: string;
         id: number;
         email: string;
+        name: string;
         password: string;
         createdAt: Date;
         updatedAt: Date;
---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { PrismaClient } from '@prisma/client'
+import userRoutes from './routes/userRoutes'
+import examRoutes from './routes/examRoutes'
+
+dotenv.config()
+const app = express()
+const prisma = new PrismaClient()
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://exam-ranking.vercel.app/'], // Adjust as needed for your frontend's URL
+    credentials: true
+}))
+app.use(express.json())
+
+//root test
+app.get("/", (req, res) => {
+    res.send("Backend is running")
+})
+
+//db test
+// app.get("/users", async(req, res) => {
+//     const users = await prisma.user.findMany()
+//     res.json(users)
+// })
+
+// attach API routes
+app.use("/api/users", userRoutes)
+app.use("/api/exams", examRoutes)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
